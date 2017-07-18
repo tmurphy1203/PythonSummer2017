02810480c86ed36cf179600164becb261c29e79b
tmurphy1203 <tmurphy1203@gmail.com>
2017-07-18 00:16:42 -0400

    Delete IceCream.py

--- IceCream.py
+++
@@ -1,8 +0,0 @@
-import Epic;
-
-ratings = []
-flavors = ["vanilla", "chocolate", "strawberry", "bacon"]
-for flavor in flavors:
-    rating = Epic.userString("Rate %s from 1 to 5:" % flavor)
-    ratings.append("%s rated as a %s" % (flavor, rating))
-    print ratings
